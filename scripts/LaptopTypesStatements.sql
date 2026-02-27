f3b8df1451b250d6b2455467f419ec2e553ad92b
pabal95 <ahmed.pabal@yahoo.com>
2026-02-27 06:20:34 -0500

    added more base files, logic not added yet for PHP files, SQL tables and INSERT statements made

--- scripts/LaptopTypesStatements.sql
+++ scripts/LaptopTypesStatements.sql
@@ -2,7 +2,15 @@ CREATE TABLE laptop_types (
     laptop_type_id      INT             NOT NULL,
     laptop_type_name    VARCHAR(255)    NOT NULL,
     laptop_type_code    VARCHAR(255)    NOT NULL UNIQUE,
+    laptop_ShelfNumber  INT             NOT NULL,
     date_time_created   TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
     date_time_updated   TIMESTAMP       DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (laptop_type_id)
 )
+
+INSERT INTO laptop_types (laptop_type_id, laptop_type_name, 
+laptop_type_code, laptop_ShelfNumber) 
+VALUES
+(1, 'Apple Laptop', 'APPLE', 1),
+(2, '2-in-1 Laptop', '2IN1', 2),
+(3, 'Chromebook', 'CHROME', 3);
--- scripts/LaptopsStatements.sql
+++ scripts/LaptopsStatements.sql
@@ -5,6 +5,9 @@ CREATE TABLE laptops (
     laptop_description TEXT            NOT NULL,
     ram                INT             NOT NULL,
     storage_capacity   INT             NOT NULL,
+    -- additional feature
+    inch_dimension     INT             NOT NULL,
+
     laptop_type_id     INT             DEFAULT 0,
     laptop_buy_price   DECIMAL(10, 2)  NOT NULL,
     laptop_sell_price  DECIMAL(10, 2)  NOT NULL,
@@ -16,3 +19,19 @@ CREATE TABLE laptops (
     ON DELETE SET NULL
     ON UPDATE CASCADE
 );
+
+--INSERT STATEMENTS
+INSERT INTO laptops (laptop_id, laptop_code, laptop_name, laptop_description, ram, 
+storage_capacity, inch_dimension, laptop_type_id, laptop_buy_price, laptop_sell_price) 
+VALUES
+(1, 'MACPRO',
+'MacBook Pro 2025', 'The latest MacBook Pro with M3 chip and improved battery life.',
+16, 512, 14, 1, 1999.99, 2499.99),
+
+(2, "LENYOGA",
+"Lenovo Yoga 2025", "A versatile 2-in-1 laptop with a flexible hinge and touchscreen.",
+16, 256, 13, 2, 1099.99, 1399.99),
+
+(3, "DELLCHROME",
+"Dell Chromebook 2025", "A lightweight and affordable Chromebook for everyday tasks.",
+8, 128, 11, 3, 299.99, 399.99);
