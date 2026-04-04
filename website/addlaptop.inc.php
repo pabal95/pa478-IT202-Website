e7616bd329d88e83a1c54147d538297c44686870
pabal95 <ahmed.pabal@yahoo.com>
2026-04-03 23:42:20 -0400

    forgot to save something..

--- website/addlaptop.inc.php
+++ website/addlaptop.inc.php
@@ -7,27 +7,26 @@
 require_once('laptop.php');
 
 if (isset($_SESSION['login'])) {
-    // Requirement 1.5: Filter values from $_POST using filter_input()
     $laptopID = filter_input(INPUT_POST, 'laptopID', FILTER_VALIDATE_INT);
     $buyPrice = filter_input(INPUT_POST, 'buyPrice', FILTER_VALIDATE_FLOAT);
     $sellPrice = filter_input(INPUT_POST, 'sellPrice', FILTER_VALIDATE_FLOAT);
     
-    // Numeric fields for constructor
+   
     $ram = filter_input(INPUT_POST, 'ram', FILTER_VALIDATE_INT);
     $storage = filter_input(INPUT_POST, 'storageCapacity', FILTER_VALIDATE_INT);
     $inch = filter_input(INPUT_POST, 'inchDimension', FILTER_VALIDATE_INT);
     $typeID = filter_input(INPUT_POST, 'laptopTypeID', FILTER_VALIDATE_INT);
 
-    // Text fields
+   
     $name = $_POST['laptopName'];
     $code = $_POST['laptopCode'];
     $desc = $_POST['laptopDescription'];
 
-    // Requirement 1.5: Ensure values are expected data types using is_int() or is_float()
+
     if (!is_int($laptopID) || !is_float($buyPrice) || !is_float($sellPrice)) {
         echo "<h2>Error: Invalid ID or Price format.</h2>";
     } else {
-        // Requirement 1.4: Script injection check
+        
         if (strpos($name, "<script") !== false || 
             strpos($code, "<script") !== false || 
             strpos($desc, "<script") !== false) {
@@ -35,7 +34,7 @@ if (isset($_SESSION['login'])) {
         } else if (Laptop::findLaptop($laptopID)) {
             echo "<h2>Error: Laptop ID #$laptopID already exists.</h2>";
         } else {
-            // Save using filtered variables
+           
             $laptop = new Laptop($laptopID, $code, $name, $desc, $ram, $storage, $inch, $typeID, $buyPrice, $sellPrice);
             $result = $laptop->saveLaptop();
             
